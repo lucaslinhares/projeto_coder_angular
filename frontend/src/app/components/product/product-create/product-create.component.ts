85fe33b746ff797f4bbc49af646b280d77d57ac7
Lucas <lucaslinhares027@gmail.com>
2022-06-10 21:06:29 -0300

    adicionando primeiro create e read

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -9,15 +10,22 @@ import { Router } from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: '',
+    price: 0.0
+  }
 
-  constructor(private ProductService: ProductService,
+  constructor(private productService: ProductService,
     private router: Router) { }
 
   ngOnInit(): void {
   }
 
   createProduct(): void {
-    this.ProductService.showMessage('Operação executada com relativo sucesso')
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto Criado')
+      this.router.navigate(['/products'])
+  })
   }
 
   cancel(): void {
